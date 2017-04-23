c08571ac5d511dcd85fb38a94b16a0812a01c90a
paul1010 <p.wu@qut.edu.au>
2017-04-24 09:57:39 +1000

    rm unnecessary files

--- testScenarios_functions.R
+++
@@ -1,9 +0,0 @@
-formatrow = function(id,scnid,node,start,end,selectstate,softevidence) {
-  # Returns formatted row for input to editevid (see DBNSMILER documentation)
-  if(selectstate=='NA') {
-    rowout = c(0,scnid,node,start,end,'NA')
-  }
-  rowout = c()
-}
-evid = editevid('add',evid,c(0,scnid,'Sediment_Quality',dpscn$Start[n2],
-                             dpscn$End[n2],sedqual$SelectState[l],'NA'),NA)
